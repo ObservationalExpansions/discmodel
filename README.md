5f1e666d02b752e69cdc593790057139e07f08a0
michael-petersen <petersen.michael.s@gmail.com>
2025-11-11 22:15:01 +0000

    add basic test package

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 **Simple exponential disc generation for testing galaxy morphology routines.**
 
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://github.com/michael-petersen/discmodel/blob/main/LICENSE)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://github.com/ObservationalExpansions/discmodel/blob/main/LICENSE)
 
 
 ## Installation
 
 Installation of `discmodel` currently proceeds from local builds after cloning this repository:
 ```
-git clone https://github.com/michael-petersen/discmodel.git
+git clone https://github.com/ObservationalExpansions/discmodel.git
 ```
 
 ```
---
+++ tests/test_discmodel.py
@@ -0,0 +1,31 @@
+import pytest
+import numpy as np
+
+import discmodel
+
+def test_discmodel_initialization():
+    """Test initialization of DiscGalaxy class."""
+    N = 1000
+    a = 3.0
+    M = 1.0
+    vcirc = 200.0
+    rmax = 30.0
+
+    disc = discmodel.DiscGalaxy(N=N, a=a, M=M, vcirc=vcirc, rmax=rmax)
+
+    assert disc.N == N
+    assert disc.a == a
+    assert disc.M == M
+    assert disc.vcirc == vcirc
+    assert disc.rmax == rmax * a
+
+    assert len(disc.x) == N
+    assert len(disc.y) == N
+    assert len(disc.z) == N
+    assert len(disc.u) == N
+    assert len(disc.v) == N
+    assert len(disc.w) == N
+
+    # Check that mass is correctly assigned
+    expected_mass = M / N
+    assert np.all(disc.mass == expected_mass)
